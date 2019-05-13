9a96abd6d0d597ff9208bfb48fd8fa9b8bf51ebc
muhamadfarkhan <muhamad.farkhan24@gmail.com>
2019-05-13 11:09:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "farkhandbserver.database.windows.net";
+    $user = "farkhan";
+    $pass = "Raihan16";
+    $db = "farkhandb";
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
